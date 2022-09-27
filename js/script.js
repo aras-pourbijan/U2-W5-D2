601806754cf120d4f28e5541cfd397440536ad7c
Aras Pourbijan <arasmidos@gmail.com>
2022-09-28 01:09:51 +0200

    not finish

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let income=prompt('whats your monthly budget for hobbies and free times?');
+document.getElementById('initBudget').innerHTML=income;
+
+let itemPrice=function addCost(price){
+    document.getElementsByClassName(item).innerHTML +=price
+}
+
+let budget=document.getElementById('totalScreen').value = income; 
+
